b624abed611008b3e0eb5e2c64f3424d5de82eb5
Brandon They <theyb5220@gmail.com>
2022-09-09 12:41:38 -0500

    Moved testing files to the app folder

--- env/script.sh
+++ env/script.sh
@@ -4,8 +4,8 @@ sudo apt-get update -y
 echo ---- Installing Python ----
 sudo apt-get install software-properties-common -y
 
-# echo ---- Moving into App ----
-# cd ../vagrant/app
+echo ---- Moving into App ----
+cd ../vagrant/app
 
-# echo ---- Starting App ----
-# python3 main.py
+echo ---- Starting App ----
+python3 main.py
